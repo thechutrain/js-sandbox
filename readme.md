17549d113456096031b86d9c5a971a79d371cb63
thechutrain <chu.chuyalan@gmail.com>
2019-07-13 15:59:48 -0400

    added readme, cleaned up code, and added default import

---
+++ readme.md
@@ -0,0 +1,17 @@
+## JS-Sandbox
+
+> node.js + babel + es6 modules + jest
+
+## Getting started:
+
+Install dependencies
+
+```
+$ npm install
+```
+
+Run `src/index.js` file
+
+```
+$ npm run dev
+```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import * as fs from 'fs';
-import { solution } from './solution';
+import { config } from './config';
 
-const INPUT_FILE = `${process.cwd()}/input.txt`;
-fs.readFile(INPUT_FILE, 'utf8', (err, content) => {
-  if (err) {
-    console.log(`There was an error reading the file ${INPUT_FILE}`);
-    return;
-  }
-  const numHouses = solution(content);
-  console.log(`Delivered pizza to: ${numHouses} house(s)`);
-});
+console.log(`On version: ${config.version}`);
